5bbc58d0705626962199a4774e33725f041dec1d
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-08-10 07:58:26 +0100

    responsiveness

--- src/Components/gameroom/Layout.jsx
+++ src/Components/gameroom/Layout.jsx
@@ -5,9 +5,13 @@ import profileIcon from "../../assets/how_to_play_img2.png";
 const Layout = ({ children }) => {
   return (
     <div className="h-screen flex bg-[#161920]">
-      <NavBar />
+      <div className="w-[64px]">
+        <div className="absolute">
+          <NavBar />
+        </div>
+      </div>
       <div className="flex-grow overflow-y-auto">
-        <div className="h-[60px] flex justify-end px-[40px] py-[24px]">
+        <div className="h-[60px] flex justify-end px-[20px] lg:px-[40px] py-[24px]">
           <button className="w-[48px] h-[48px] rounded-full overflow-hidden relative">
             <Image
               src={profileIcon}
