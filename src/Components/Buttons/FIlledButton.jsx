d44c58e408c3689ebc4e3b7ff76eb94df5ca9f70
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-08-10 06:27:56 +0100

    added path for button click

--- src/Components/Buttons/FIlledButton.jsx
+++ src/Components/Buttons/FIlledButton.jsx
@@ -1,11 +1,20 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const FilledButton = ({ text, img }) => {
-  return <button className="font-nunito font-[700] bg-gradient-to-r from-[#9068C1] to-[#3892DB] border border-transparent hover:border-purple-400 hover:to-[#9068C1] hover:from-[#3892DB] transition-all duration-500 ease-in-out px-[29px] py-[10px] rounded-[6px] cursor-pointer text-[#fefefe] flex gap-2 items-center justify-center">
-    
-    {img && <Image src={img} alt={text} width={20} height={20} /> }
-    {text}
-  </button>;
+const FilledButton = (props) => {
+  const { text, img, link } = props
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => router.push(link)}
+      className="font-nunito font-[700] bg-gradient-to-r from-[#9068C1] to-[#3892DB] border border-transparent hover:border-purple-400 hover:to-[#9068C1] hover:from-[#3892DB] transition-all duration-500 ease-in-out px-[29px] py-[10px] rounded-[6px] cursor-pointer text-[#fefefe] flex gap-2 items-center justify-center"
+    >
+      {img && <Image src={img} alt={text} width={20} height={20} />}
+      {text}
+    </button>
+  );
 };
 
 export default FilledButton;
--- src/Components/Buttons/OutlinedButton.jsx
+++ src/Components/Buttons/OutlinedButton.jsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
 
-const OutlinedButton = ({text}) => {
-  return (
-    <button className="outline-btn font-nunito font-[700] px-[29px] py-[10px] cursor-pointer text-[#fefefe]">{text}</button>
-  )
-}
+const OutlinedButton = ({ text, link }) => {
+  if (link) {
+    return (
+      <Link href={link}>
+        <button className="outline-btn font-nunito font-[700] px-[29px] py-[10px] cursor-pointer text-[#fefefe]">
+          {text}
+        </button>
+      </Link>
+    );
+  } else {
+    return (
+      <button className="outline-btn font-nunito font-[700] px-[29px] py-[10px] cursor-pointer text-[#fefefe]">
+        {text}
+      </button>
+    );
+  }
+};
 
-export default OutlinedButton
+export default OutlinedButton;
