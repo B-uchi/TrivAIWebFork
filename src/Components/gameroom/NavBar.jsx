381f59fbc1b05d2e7c4790d18be9b40c869afe59
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-08-10 07:58:16 +0100

    responsiveness

--- src/Components/gameroom/NavBar.jsx
+++ src/Components/gameroom/NavBar.jsx
@@ -1,26 +1,78 @@
+"use client";
 import { HiMenu } from "react-icons/hi";
 import { LuPlus } from "react-icons/lu";
 import { IoIosHelpCircle } from "react-icons/io";
 import { IoBug } from "react-icons/io5";
+import { useState } from "react";
+import { RxCaretUp } from "react-icons/rx";
+import { useRouter } from "next/navigation";
 
 const NavBar = () => {
+  const router = useRouter();
+  const [expandMenu, setExpandMenu] = useState(false);
+  const style = expandMenu ? "w-[224px] bg-[#161920]" : "w-[57px] bg-[#161920]";
   return (
-    <nav className="w-[64px]">
-      <div className="gradient-outline h-screen flex flex-col justify-between items-center py-8">
-        <div className="flex flex-col gap-[32px] items-center">
-          <button>
-            <HiMenu size={38} color="#fefefe" />
-          </button>
-          <button className="bg-[#000000] w-[32px] h-[32px] bg-opacity-[38%] rounded-full p-1">
-            <LuPlus color="#fefefe" size={19} className="mx-auto" />
-          </button>
+    <nav className={style}>
+      <div
+        className={`gradient-outline h-screen flex flex-col justify-between ${
+          expandMenu ? "" : "items-center"
+        } py-8 px-4`}
+      >
+        <div className="">
+          <div className="flex flex-col gap-[32px] ">
+            <button onClick={() => setExpandMenu(!expandMenu)}>
+              <HiMenu size={38} color="#fefefe" />
+            </button>
+            <button
+              onClick={() => router.push("/create-a-game")}
+              className={`bg-[#000000] ${
+                expandMenu
+                  ? "gap-[10px] items-center"
+                  : "w-[32px] h-[32px] items-center"
+              } bg-opacity-[38%] rounded-full p-1 flex`}
+            >
+              <LuPlus
+                color="#fefefe"
+                size={19}
+                className={`${expandMenu ? "ml-2" : "mx-auto"}`}
+              />
+              {expandMenu && (
+                <p className="font-nunito font-[600] text-[20px] text-[#fefefe]">
+                  Create a game
+                </p>
+              )}
+            </button>
+          </div>
+          {expandMenu && (
+            <div className="mt-[60px]">
+              <div className="flex items-center gap-[20px]">
+                <p className="font-nunito text-[#fefefe] text-[18px]">
+                  Recent Games
+                </p>
+                <button>
+                  <RxCaretUp color="#fefefe" size={28} />
+                </button>
+              </div>
+              {/* <div className="">
+              <p className="font-nunito text-[#fefefe] text-[18px]">
+                
+              </p>
+            </div> */}
+            </div>
+          )}
         </div>
-        <div className="mb-0 flex flex-col gap-[32px] items-center text-[#878B8A]">
-          <button>
-            <IoIosHelpCircle size={24}/>
+        <div className={`mb-0 flex flex-col gap-[32px] text-[#878B8A]`}>
+          <button className="flex items-center gap-[10px]">
+            <IoIosHelpCircle size={24} />
+            {expandMenu && (
+              <p className="font-[600] text-[16px] font-nunito">Get Help</p>
+            )}
           </button>
-          <button>
-            <IoBug size={24}/>
+          <button className="flex items-center gap-[10px]">
+            <IoBug size={24} />
+            {expandMenu && (
+              <p className="font-[600] text-[16px] font-nunito">Report Bug</p>
+            )}
           </button>
         </div>
       </div>
--- src/app/create-a-game/page.jsx
+++ src/app/create-a-game/page.jsx
@@ -13,36 +13,36 @@ const CreateGame = () => {
     <Layout>
       <div className=" flex flex-col items-center mb-10">
         <div className="gap-[12px] text-center mt-5">
-          <h1 className="font-fredoka font-[600] gradient-text text-[50px] ">
+          <h1 className="font-fredoka font-[600] gradient-text lg:text-[50px] text-[30px] ">
             Create New Game
           </h1>
-          <p className="font-nunito font-[400] text-[28px] text-[#878B8A]">
+          <p className="font-nunito font-[400] lg:text-[28px] text-[#878B8A]">
             Customize your trivia game and invite players to join.
           </p>
         </div>
-        <div className="mt-8 gradient-outline lg:w-[800px] w-[90%] p-[50px]">
-          <h2 className="font-fredoka font-[500] text-[25px] gradient-text text-center">
+        <div className="mt-8 gradient-outline lg:w-[800px] w-[90%] lg:p-[50px] p-[20px]">
+          <h2 className="font-fredoka font-[500] text-[20px] lg:text-[25px] gradient-text text-center">
             Game Details
           </h2>
           <div className="mt-10 flex flex-col gap-[30px]">
             <div className="flex flex-col gap-[8px]">
-              <p className="text-[18px] font-poppins font-[500] text-[#fefefe]">
+              <p className="lg:text-[18px] font-poppins font-[500] text-[#fefefe]">
                 Game Name
               </p>
               <input
                 type="text"
-                className="gradient-outline bg-transparent p-[20px] w-full font-poppins font-[400]"
+                className="gradient-outline bg-transparent lg:p-[20px] p-[5px] w-full font-poppins font-[400]"
                 placeholder="Enter game name"
               />
             </div>
             <div className="flex flex-col gap-[8px]">
-              <p className="text-[18px] font-poppins font-[500] text-[#fefefe]">
+              <p className="lg:text-[18px] font-poppins font-[500] text-[#fefefe]">
                 Category
               </p>
 
               <select
                 defaultValue={"selectCategory"}
-                className="gradient-outline bg-transparent p-[20px] w-full font-poppins font-[400] text-[#9A9A9A] gragient-text"
+                className="gradient-outline bg-transparent p-[5px] lg:p-[20px] w-full font-poppins font-[400] text-[#9A9A9A] gragient-text"
               >
                 <option value="selectCategory" selected disabled>
                   Select Category
@@ -53,12 +53,12 @@ const CreateGame = () => {
                 <option value="Science">Science</option>
               </select>
             </div>
-            <div className="flex justify-between">
+            <div className="flex lg:flex-row flex-col lg:gap-0 gap-[32px] justify-between">
               <div className="">
-                <p className="text-[18px] font-poppins font-[500] text-[#fefefe]">
+                <p className="lg:text-[18px] font-poppins font-[500] text-[#fefefe]">
                   Game Duration/min
                 </p>
-                <div className="w-full h-[50px] gradient-outline flex">
+                <div className="w-full lg:h-[50px] h-[40px] gradient-outline flex">
                   <button
                     onClick={() =>
                       setDuration(duration == 1 ? 1 : duration - 1)
@@ -79,10 +79,10 @@ const CreateGame = () => {
                 </div>
               </div>
               <div className="">
-                <p className="text-[18px] font-poppins font-[500] text-[#fefefe]">
+                <p className="lg:text-[18px] font-poppins font-[500] text-[#fefefe]">
                   Number of Players
                 </p>
-                <div className="w-full h-[50px] gradient-outline flex">
+                <div className="w-full lg:h-[50px] h-[40px] gradient-outline flex">
                   <button
                     onClick={() =>
                       setNoOfPlayers(noOfPlayers == 1 ? 1 : noOfPlayers - 1)
@@ -103,7 +103,7 @@ const CreateGame = () => {
                 </div>
               </div>
             </div>
-            <button className="font-[700] rounded-[10px] font-nunito text-[18px] text-[#fefefe] bg-custom p-[20px]">Create Game</button>
+            <button className="font-[700] rounded-[10px] font-nunito text-[18px] text-[#fefefe] bg-custom p-[10px] lg:p-[20px]">Create Game</button>
           </div>
         </div>
       </div>
