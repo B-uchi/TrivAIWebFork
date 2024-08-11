78b67ceb43b743b548d7babc0efc81f442b00001
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-08-11 20:52:13 +0100

    modified toolbar

--- src/Components/gameroom/Layout.jsx
+++ src/Components/gameroom/Layout.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./NavBar";
 import Image from "next/image";
 import profileIcon from "../../assets/how_to_play_img2.png";
+import { Toaster } from "sonner";
 
 const Layout = ({ children }) => {
   return (
@@ -10,15 +11,19 @@ const Layout = ({ children }) => {
           <NavBar />
         </div>
       </div>
-      <div className="flex-grow overflow-y-auto">
-        <div className="h-[60px] flex justify-end px-[20px] lg:px-[40px] py-[24px]">
+      <div className="flex flex-col w-full flex-1 overflow-y-auto">
+        <Toaster richColors position="top-right" />
+        <div className="h-[40px] flex items-center justify-between px-[20px] lg:px-[40px] py-[28px]">
+          <h1 className="font-[600] font-fredoka text-[28px] gradient-text">
+            TrivAi
+          </h1>
           <button className="w-[48px] h-[48px] rounded-full overflow-hidden relative">
             <Image
               src={profileIcon}
               alt="Profile Icon"
               layout="fill"
               objectFit="cover"
-              className="rounded-full"
+              className="rounded-full top-0"
             />
           </button>
         </div>
