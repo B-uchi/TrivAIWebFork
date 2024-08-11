a3dff3069743a48e25496b2c1b6bacb00e723e68
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-08-11 20:49:46 +0100

    create a game screen

--- src/app/create-a-game/page.jsx
+++ src/app/create-a-game/page.jsx
@@ -1,17 +1,62 @@
 "use client";
 import { Layout } from "@/Components/gameroom";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import {
   IoCaretBackCircleOutline,
   IoCaretForwardCircleOutline,
 } from "react-icons/io5";
+import { toast } from "sonner";
 
 const CreateGame = () => {
-  const [duration, setDuration] = useState(1);
-  const [noOfPlayers, setNoOfPlayers] = useState(1);
+  const [noOfQuestions, setNoOfQuestions] = useState(1);
+  const [category, setCategory] = useState("selectCategory");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter()
+
+  const createGame = async () => {
+    setLoading(true);
+    if (category == "selectCategory") {
+      setLoading(false);
+      return "Please select a valid category.";
+    } else {
+      try {
+        const request = await fetch(
+          "https://trivai-backend.onrender.com/gameroom/create",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              category: category,
+              no_of_questions: noOfQuestions,
+            }),
+            headers: {
+              X_API_Token: process.env.NEXT_PUBLIC_API_KEY,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (request.status == 201) {
+          setLoading(false);
+          const data = await request.json();
+          toast.success("Game created successfully");
+          sessionStorage.clear()
+          sessionStorage.setItem("room_id", data.room_id);
+          sessionStorage.setItem("moderator_token", data.moderator_token);
+          router.push('/moderator-waitroom')
+        } else {
+          setLoading(false);
+          return toast.error("An error occured.");
+        }
+      } catch (error) {
+        setLoading(false);
+        return toast.error("Request incomplete.");
+      }
+    }
+  };
+
   return (
     <Layout>
-      <div className=" flex flex-col items-center mb-10">
+      <div className=" flex flex-col items-center">
         <div className="gap-[12px] text-center mt-5">
           <h1 className="font-fredoka font-[600] gradient-text lg:text-[50px] text-[30px] ">
             Create New Game
@@ -20,31 +65,22 @@ const CreateGame = () => {
             Customize your trivia game and invite players to join.
           </p>
         </div>
-        <div className="mt-8 gradient-outline lg:w-[800px] w-[90%] lg:p-[50px] p-[20px]">
+        <div className="mt-8 gradient-outline lg:w-[800px] w-[90%] lg:p-[40px] p-[20px]">
           <h2 className="font-fredoka font-[500] text-[20px] lg:text-[25px] gradient-text text-center">
             Game Details
           </h2>
           <div className="mt-10 flex flex-col gap-[30px]">
             <div className="flex flex-col gap-[8px]">
               <p className="lg:text-[18px] font-poppins font-[500] text-[#fefefe]">
-                Game Name
-              </p>
-              <input
-                type="text"
-                className="gradient-outline bg-transparent lg:p-[20px] p-[5px] w-full font-poppins font-[400]"
-                placeholder="Enter game name"
-              />
-            </div>
-            <div className="flex flex-col gap-[8px]">
-              <p className="lg:text-[18px] font-poppins font-[500] text-[#fefefe]">
                 Category
               </p>
 
               <select
-                defaultValue={"selectCategory"}
-                className="gradient-outline bg-transparent p-[5px] lg:p-[20px] w-full font-poppins font-[400] text-[#9A9A9A] gragient-text"
+                defaultValue={category}
+                onChange={(e) => setCategory(e.target.value)}
+                className="gradient-outline bg-transparent p-[5px] lg:p-[20px] w-full font-poppins font-[400] text-[#9A9A9A]"
               >
-                <option value="selectCategory" selected disabled>
+                <option value="selectCategory" disabled>
                   Select Category
                 </option>
                 <option value="Movies">Movies</option>
@@ -54,48 +90,26 @@ const CreateGame = () => {
               </select>
             </div>
             <div className="flex lg:flex-row flex-col lg:gap-0 gap-[32px] justify-between">
-              <div className="">
+              <div className="flex flex-col gap-[8px]">
                 <p className="lg:text-[18px] font-poppins font-[500] text-[#fefefe]">
-                  Game Duration/min
+                  Number of Questions
                 </p>
                 <div className="w-full lg:h-[50px] h-[40px] gradient-outline flex">
                   <button
                     onClick={() =>
-                      setDuration(duration == 1 ? 1 : duration - 1)
+                      setNoOfQuestions(
+                        noOfQuestions == 1 ? 1 : noOfQuestions - 1
+                      )
                     }
                     className="h-full w-[25%] flex justify-center items-center gradient-right"
                   >
                     <IoCaretBackCircleOutline color="#fefefe" size={20} />
                   </button>
                   <div className="flex-grow text-center font-poppins gradient-text h-full flex justify-center items-center">
-                    <p>{duration}</p>
+                    <p>{noOfQuestions}</p>
                   </div>
                   <button
-                    onClick={() => setDuration(duration + 1)}
-                    className="h-full w-[25%] flex justify-center items-center gradient-left"
-                  >
-                    <IoCaretForwardCircleOutline color="#fefefe" size={20} />
-                  </button>
-                </div>
-              </div>
-              <div className="">
-                <p className="lg:text-[18px] font-poppins font-[500] text-[#fefefe]">
-                  Number of Players
-                </p>
-                <div className="w-full lg:h-[50px] h-[40px] gradient-outline flex">
-                  <button
-                    onClick={() =>
-                      setNoOfPlayers(noOfPlayers == 1 ? 1 : noOfPlayers - 1)
-                    }
-                    className="h-full w-[25%] flex justify-center items-center gradient-right"
-                  >
-                    <IoCaretBackCircleOutline color="#fefefe" size={20} />
-                  </button>
-                  <div className="flex-grow text-center font-poppins gradient-text h-full flex justify-center items-center">
-                    <p>{noOfPlayers}</p>
-                  </div>
-                  <button
-                    onClick={() => setNoOfPlayers(noOfPlayers + 1)}
+                    onClick={() => setNoOfQuestions(noOfQuestions + 1)}
                     className="h-full w-[25%] flex justify-center items-center gradient-left"
                   >
                     <IoCaretForwardCircleOutline color="#fefefe" size={20} />
@@ -103,7 +117,12 @@ const CreateGame = () => {
                 </div>
               </div>
             </div>
-            <button className="font-[700] rounded-[10px] font-nunito text-[18px] text-[#fefefe] bg-custom p-[10px] lg:p-[20px]">Create Game</button>
+            <button
+              onClick={() => createGame()}
+              className="font-[700] rounded-[10px] font-nunito text-[18px] text-[#fefefe] bg-custom p-[10px] lg:p-[20px] flex items-center justify-center gap-2"
+            >
+              Create Game {loading && <div className="loader"></div>}
+            </button>
           </div>
         </div>
       </div>
